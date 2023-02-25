064c4465edb71a49fcf14d5c428192633dc566b0
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-02-25 14:14:01 -0300

    add: mudando a conexao do mongodb

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 if(process.env.NODE_ENV == "production"){
     module.exports  = {mongoURI: "mongodb+srv://liliu:<#liliuchiha3a>@cluster0-nbec3.mongodb.net/test?retryWrites=true"}
 }else{
-    module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+    //module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+    module.exports = {mongoURI: "mongodb://mongoadmin:mongo123@localhost:27017/blogapp?authSource=admin"}
 }
